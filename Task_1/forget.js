78a4c0cbdfee7f9f71e42341eb4a3491d93d251c
gaurav malik <malikgaurav080@gmail.com>
2022-11-14 21:46:04 +0530

    add DYNmic form

--- Task_1/forget.js
+++ Task_1/forget.js
@@ -2,7 +2,6 @@ function submit(){
     var email=document.getElementById("email").value;
     var number=document.getElementById("number").value;
     var city=document.getElementById("city").value;
-    
     fetch("http://localhost:3000/users",{
         method:"GET",
         headers:{
@@ -11,7 +10,8 @@ function submit(){
     }).then(response=>response.json())
     .then(json=>{
         json.map(data=>{
-            if(data.Email==email && data.Phone-NO==number && data.City==city){
+            if(data.Email==email && data.PhoneNO==number && data.City==city){
+                pass+= data.Password
                 alert("your password: ",data.Password);
                 flag = 1
                 return
